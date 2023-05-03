93fec1ab13e9fbdd5b2f19ffd107514c7748a3e5
daniel aldana <danielaldana121@gmail.com>
2023-05-02 23:09:30 -0500

    quite el router para probar el despliegue y quite

--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -5,9 +5,6 @@ import validateUserMiddle from "../middlewares/validateUserMiddleware";
 const controller = new UsersController;
 const router = Router();
 
-router.get('/prueba', (req, res) => {
-    res.send('Hola desde el despiegue segunda prueba')
-});
 router.post("/register", validateUserMiddle, controller.registerUser);
 router.post("/login");
 
