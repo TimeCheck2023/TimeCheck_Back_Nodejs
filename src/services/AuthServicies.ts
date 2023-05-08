62260ab950f05e1956e517c06f6a08801e0eacfe
daniel aldana <danielaldana121@gmail.com>
2023-05-07 21:50:44 -0500

    cree un objeto que devuelve el Auth

--- src/services/AuthServicies.ts
+++ src/services/AuthServicies.ts
@@ -11,8 +11,15 @@ class Auth_service {
                 .input("correo_usu_org", sql.VarChar(255), emailAddress)
             const responde = await request.execute(query.VeryUsersLogin)
             if (!await macthPass(password, responde.recordset[0].contraseña)) throw new Error("usuario o contraseña incorrect");
-            const token = await CreateToken(responde.recordset[0].correo, responde.recordset[0].EsUsuario)
-            return token;
+            const correo = responde.recordset[0].correo;
+            const EsUsuario = responde.recordset[0].EsUsuario 
+            const token = await CreateToken(correo, EsUsuario)
+            const data = {
+                token,
+                correo, 
+                EsUsuario
+            }
+            return data;
         } catch (error) {
             throw error
         }
