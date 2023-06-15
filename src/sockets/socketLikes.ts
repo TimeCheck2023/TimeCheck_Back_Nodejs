78154f74a803ad9f01f46a9c0c211de24454208c
danie aldana <danielaldana212@gmail.com>
2023-06-15 13:51:59 -0500

    creando las graficas de details de likes y comments

--- src/sockets/socketLikes.ts
+++ src/sockets/socketLikes.ts
@@ -65,8 +65,7 @@ export class Socket_io_Likes {
           id_evento5
         );
       const result = await request.execute(querys.getCountLikes);
-      console.log(result.recordset[0]['']);
-      console.log(result.recordsets[0] as object);
+      console.log(result);
       
       this.io.emit("Countlikes", result.recordset);
     } catch (error) {
